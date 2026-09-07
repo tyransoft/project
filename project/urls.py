ea96a9d25ed76defd5633f3e9be1b68f810abfe5
tyransoft <tyransoftagency@gmail.com>
2026-09-07 12:23:39 +0200

    3

--- project/urls.py
+++ project/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
 ]
-if  settings.DEBUG:
+if not settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
