a60e46c857387cfabc36e8bdfdd2dc80e04c19cd
tyransoft <tyransoftagency@gmail.com>
2026-09-08 21:26:09 +0200

    1234

--- core/views.py
+++ core/views.py
@@ -66,10 +66,9 @@ def dashboard(request):
 
 
 def product_detail(request, slug):
-    product = get_object_or_404(Product, slug=slug, is_active=True)
+    product = get_object_or_404(Product, slug=slug)
     related_products = Product.objects.filter(
         category=product.category, 
-        is_active=True
     ).exclude(id=product.id)[:4]
     
     suggested_handles = Handle.objects.all().order_by('-created_at')[:4]
