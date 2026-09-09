64fecff8e52f2c70c72337ccab0de6b7a8cc0002
tyransoft <tyransoftagency@gmail.com>
2026-09-09 18:12:00 +0200

    رلف

--- project/settings.py
+++ project/settings.py
@@ -28,15 +28,11 @@ SECRET_KEY = 'django-insecure-qmh+!0(xi&6w1fx2ai$ziih_&vp4@255&r$o$ysk0wc)vw#hbu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 ALLOWED_HOSTS = [
-    "bayti.tip2.libyanspider.cloud",
-    "baytidoors.ly",
-    "www.baytidoors.ly",
+    "baytidoors.ly"
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://baytidoors.ly",
-    "https://www.baytidoors.ly",
-    "https://bayti.tip2.libyanspider.cloud",
+    "https://baytidoors.ly"
 ]
 
 SITE_URL = "https://baytidoors.ly"
